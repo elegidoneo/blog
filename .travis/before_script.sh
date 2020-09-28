cebfaca1931045ea73ea039498639a988ef12946
Gustavo Pino <gustavo.pino@colppy.com>
2020-09-28 00:32:01 -0300

    cambios

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-
-echo -e "Creating database...\n"
-mysql -e 'CREATE DATABASE IF NOT EXISTS `'$DATABASE_NAME'` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
+if [[ -v DATABASE_NAME ]];
+then
+    echo -e "Creating database...\n"
+    mysql -e 'CREATE DATABASE IF NOT EXISTS `'${DATABASE_NAME}'` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
+fi
+if [[ -v GITHUB_TOKEN ]];
+then
+    echo -e "Add github token...\n"
+    composer config github-oauth.github.com ${GITHUB_TOKEN}
+fi
 cp .travis/.env .env
 COMPOSER_MEMORY_LIMIT=-1 composer install
 mkdir -p build/logs
